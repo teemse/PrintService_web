cc0faa7436f1ba315b9e5325479544252edc7aa7
teemse <chernikov.artem2000@gmail.com>
2023-07-02 15:02:19 +0300

    Edit "Experience"

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -1,20 +1,20 @@
 window.onload = function () {
-  const favicon = document.getElementById('favicon')
-  const pageTitle = document.title
-  const attentionMessage = 'Come back'
+  const favicon = document.getElementById("favicon");
+  const pageTitle = document.title;
+  const attentionMessage = "Вернитесь пожалуйста";
 
-  document.addEventListener('visibilitychange', function (e) {
-    const isPageActive = !document.hidden
-    toggle(isPageActive)
-  })
+  document.addEventListener("visibilitychange", function (e) {
+    const isPageActive = !document.hidden;
+    toggle(isPageActive);
+  });
 
   function toggle(isPageActive) {
     if (isPageActive) {
-      document.title = pageTitle
-      favicon.href = './assets/images/dp_male.svg'
+      document.title = pageTitle;
+      favicon.href = "./assets/images/dp_male.svg";
     } else {
-      document.title = attentionMessage
-      favicon.href = './assets/images/folded.png'
+      document.title = attentionMessage;
+      favicon.href = "./assets/images/folded.png";
     }
   }
-}
+};
