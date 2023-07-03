05503f747388c35d90f0f934e0babfcf2aae83bb
teemse <31262294+teemse@users.noreply.github.com>
2023-07-03 16:06:38 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,10 +22,10 @@ let header = $(`
 
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Главная</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Наш опыт</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Проэкты</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Прайс-лист</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
    <li class="nav-item">
