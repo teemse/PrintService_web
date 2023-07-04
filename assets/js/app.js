65d9d114c3b7509b1e2eb689552676edf551ed99
teemse <31262294+teemse@users.noreply.github.com>
2023-07-04 18:40:15 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,11 +23,11 @@ let header = $(`
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Главная</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Наш опыт</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">О нас</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Услуги</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Прайс-лист</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Контакты</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Блог</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Telegram</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
