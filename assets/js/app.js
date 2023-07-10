1bfd8990d809c8cf8c41bf17f7246d4f9780f095
teemse <chernikov.artem2000@gmail.com>
2023-07-10 15:33:05 +0300

    Analitics

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,7 @@ let header = $(`
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Главная</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">О нас</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Прайс-лист</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Цены</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Услуги</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Контакты</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Telegram</a></li>
